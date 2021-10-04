36994732c58e3e2ca6c9e65dc64cb40495ce6e92
samagd <sam.agd@outlook.com>
2021-10-04 14:08:28 +0200

    Creating Note Model

---
+++ app/src/main/java/com/example/mynoteappcourse/feature_note/domain/model/Note.kt
@@ -0,0 +1,19 @@
+package com.example.mynoteappcourse.feature_note.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.mynoteappcourse.ui.theme.*
+
+@Entity
+data class Note (
+    val title: String,
+    val content: String,
+    val timeStamp: Long,
+    @PrimaryKey val id: Int?= null
+    )
+{
+    companion object{
+        val noteColors= listOf(RedOrange, LightGreen, Violet, BabyBlue, RedPink)
+    }
+
+}
