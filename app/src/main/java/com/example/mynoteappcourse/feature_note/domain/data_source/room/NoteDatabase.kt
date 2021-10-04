ea0df70be63e1f314167bae15083ab5f7045f6f3
samagd <sam.agd@outlook.com>
2021-10-04 23:07:16 +0200

    Creating NoteDatabase class

---
+++ app/src/main/java/com/example/mynoteappcourse/feature_note/domain/data_source/room/NoteDatabase.kt
@@ -0,0 +1,14 @@
+package com.example.mynoteappcourse.feature_note.domain.data_source.room
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.mynoteappcourse.feature_note.domain.model.Note
+
+@Database(
+    version = 1,
+    entities = [Note::class]
+)
+abstract class NoteDatabase: RoomDatabase() {
+
+    abstract val noteDao: NoteDao
+}
