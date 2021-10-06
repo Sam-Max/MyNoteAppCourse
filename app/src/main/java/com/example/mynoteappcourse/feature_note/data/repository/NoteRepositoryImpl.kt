7a1eea9cc729efbd781fa9cbb22fca841aec0117
samagd <sam.agd@outlook.com>
2021-10-06 15:30:49 +0200

    Creating NoteRepositoy Interface and Implementation

---
+++ app/src/main/java/com/example/mynoteappcourse/feature_note/data/repository/NoteRepositoryImpl.kt
@@ -0,0 +1,24 @@
+package com.example.mynoteappcourse.feature_note.data.repository
+
+import com.example.mynoteappcourse.feature_note.domain.model.Note
+import com.example.mynoteappcourse.feature_note.domain.repository.NoteRepository
+import kotlinx.coroutines.flow.Flow
+
+class NoteRepositoryImpl: NoteRepository {
+
+    override fun getNotes(): Flow<List<Note>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getNoteById(id: Int): Note? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteNote(note: Note) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun insertNote(note: Note) {
+        TODO("Not yet implemented")
+    }
+}
