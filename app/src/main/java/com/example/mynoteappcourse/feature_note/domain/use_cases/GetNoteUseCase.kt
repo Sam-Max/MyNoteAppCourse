a321fde1a6273b75f40ade8171c7e37deb6b2ce5
samagd <sam.agd@outlook.com>
2021-10-15 23:10:32 +0200

    Creating UsesCase for Getting a Note for Edit Screen

---
+++ app/src/main/java/com/example/mynoteappcourse/feature_note/domain/use_cases/GetNoteUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.mynoteappcourse.feature_note.domain.use_cases
+
+import com.example.mynoteappcourse.feature_note.domain.model.Note
+import com.example.mynoteappcourse.feature_note.domain.repository.NoteRepository
+
+class GetNoteUseCase(
+    private val repository: NoteRepository
+) {
+
+    suspend operator fun invoke(id: Int): Note?{
+        return repository.getNoteById(id)
+    }
+}
